4b92e0d4c157da724006644de205c436493ebf04
Sushmitha-B-au50 <82799245+Sushmitha-B-au50@users.noreply.github.com>
2023-05-17 22:17:46 +0530

    Update API.js

--- medicineapp/src/API.js
+++ medicineapp/src/API.js
@@ -1,5 +1,5 @@
 import  axios from 'axios'
 
-// export const API = axios.create({baseURL:'https://ecommerce-api-8o6u.onrender.com/'});
-export const API = axios.create({baseURL:'http://localhost:8001/'});
+ export const API = axios.create({baseURL:'https://medicineapp-api.onrender.com/'});
+//export const API = axios.create({baseURL:'http://localhost:8001/'});
 
